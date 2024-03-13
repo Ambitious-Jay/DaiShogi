82f9b42020b23ddb79763474122fee6e116114d6
Amibitious-Jay <andrewjo257@gmail.com>
2024-03-12 23:19:20 -0700

    Initial Commit

---
+++ DaiShogi/DaiShogiApp.swift
@@ -0,0 +1,17 @@
+//
+//  DaiShogiApp.swift
+//  DaiShogi
+//
+//  Created by Andrew Jo on 3/12/24.
+//
+
+import SwiftUI
+
+@main
+struct DaiShogiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
