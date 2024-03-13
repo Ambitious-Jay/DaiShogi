f8335d62a927786a8d9df8fd4852a0d4756efdb2
snagglegrolop <75964752+snagglegrolop@users.noreply.github.com>
2024-03-13 11:00:19 -0700

    Initial Commit

---
+++ daishogi/daishogiApp.swift
@@ -0,0 +1,17 @@
+//
+//  daishogiApp.swift
+//  daishogi
+//
+//  Created by Zachary Abrahamson  on 3/13/24.
+//
+
+import SwiftUI
+
+@main
+struct daishogiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
