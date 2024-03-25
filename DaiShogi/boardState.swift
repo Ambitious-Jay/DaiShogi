7378a5c800118e11bf5e0a60b13cbbd86e5c20d6
snagglegrolop <75964752+snagglegrolop@users.noreply.github.com>
2024-03-24 21:20:40 -0700

    tile pieces added, not functional yet

--- DaiShogi/boardState.swift
+++ DaiShogi/boardState.swift
@@ -10,15 +10,13 @@ import SwiftUI
 
 
 class boardState: ObservableObject {
-    @Published var boardLayout: [[tileView]]
+    @Published var boardLayout: [tileView]
     init() {
-        var initialBoardState = [[tileView]]()
+        var initialBoardState = [tileView]()
         for row in 1..<16 {
-            var rowState = [tileView]()
             for col in 1..<16 {
-                rowState.append(tileView(posOnBoard: (row: row, col: col)))
+                initialBoardState.append(tileView(posOnBoard: (row: row, col: col)))
             }
-            initialBoardState.append(rowState)
         }
         self.boardLayout = initialBoardState
     }
--- DaiShogi/pieceStruct.swift
+++ DaiShogi/pieceStruct.swift
@@ -6,11 +6,15 @@
 //
 
 import Foundation
+import SwiftUI
 
 protocol Piece {
     var isWhite: Bool { get set }
     var pieceID: Int { get set }
+    var pieceName: String { get set }
     var position: Int { get set }
+    
+    var chipView: any View { get set }
     
     func calculateMoves()
     
@@ -21,5 +25,22 @@ extension Piece {
     func movePiece(newSpot: Int) {
 //        self.position = newSpot
     }
+    
+//    var chipView =
 }
 
+struct tileLook: View {
+    let tileName: String
+    let style: RoundedCornerStyle
+    var body: some View {
+        UnevenRoundedRectangle(cornerRadii: RectangleCornerRadii(topLeading: 13, bottomLeading: 2, bottomTrailing: 2, topTrailing: 13), style: .circular)
+            .fill(Color.darkBkg)
+            .shadow(radius: 5, x: 0.8, y: 0.9)
+            .frame(width: 35, height: 45)
+            .overlay {
+                Text(tileName)
+                    .minimumScaleFactor(0.1)
+                    .foregroundStyle(.black)
+            }
+    }
+}
